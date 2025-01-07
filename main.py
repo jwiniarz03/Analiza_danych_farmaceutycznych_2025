3a00d5dc27235c17f85891880c979722346e8d17
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-07 21:24:52 +0100

    added data frame with basic info about drugs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from data_processing.data_loader import load_basic_drug_data
+from data_processing.data_frames import create_data_frame_basic_info
 
 if __name__ == "__main__":
 
@@ -6,3 +7,5 @@ if __name__ == "__main__":
 
     # Load and parse data
     drugs_data = load_basic_drug_data(file_path)
+
+    drug_df = create_data_frame_basic_info(drugs_data)
