70bd87f4bd0b88a02a8e20ca6b9b36cc49ab7074
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-07 21:24:23 +0100

    added data frame with basic info about drugs

---
+++ data_processing/data_frames.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+
+def create_data_frame_basic_info(drugs_data: dict) -> pd.DataFrame:
+    """
+    Create a pandas DataFrame from thedrug data dictionary.
+
+    Args:
+        drugs_data (dict): Dictionary with drug id as keys and drug information as values.
+
+    Returns:
+        pd.DataFrame: DataFrame with drug data.
+    """
+
+    return (
+        pd.DataFrame.from_dict(drugs_data, orient="index")
+        .reset_index()
+        .rename(columns={"index": "id"})
+    )
