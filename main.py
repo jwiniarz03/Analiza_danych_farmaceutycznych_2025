d9babd3cc2d36328ec2dc75371b4bbd1ff37bbbe
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-13 13:21:25 +0100

    changed names

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from data_processing.data_loader import load_basic_drug_data
-from data_processing.data_frames import create_data_frame_basic_info
+from data_processing.data_loader import load_basic_drug_data, load_drug_synonyms_data
+from data_processing.data_frames import (
+    create_data_frame_basic_info,
+    create_data_frame_synonyms,
+)
 
 if __name__ == "__main__":
 
@@ -9,3 +12,7 @@ if __name__ == "__main__":
     drugs_data = load_basic_drug_data(file_path)
 
     drug_df = create_data_frame_basic_info(drugs_data)
+
+    synonyms_data = load_drug_synonyms_data(file_path)
+
+    synonyms_df = create_data_frame_synonyms(synonyms_data)
