15ff01dc48d562b006fd74c65b5aa66b255973d1
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-14 15:35:24 +0100

    addeed plot for graph - todo: better look

--- visualisations/graphs.py
+++ visualisations/graphs.py
@@ -1,6 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
+import textwrap
 
 
 def generate_synonyms_graph(drug_id: str, synonyms_df: pd.DataFrame) -> nx.Graph:
@@ -34,3 +35,30 @@ def generate_synonyms_graph(drug_id: str, synonyms_df: pd.DataFrame) -> nx.Graph
         G.add_edge(drug_id, synonym)
 
     return G
+
+
+def plot_synonyms_graph(G: nx.Graph, drug_id: str):
+    """
+    Generate matplotlib plot of synonyms for the given DrugBank ID.
+
+    Args:
+        G (nx.Graph): The graph to plot.
+        drug_id (str): The DrugBank ID of which the graph is generated.
+    """
+
+    plt.figure(figsize=(8, 6))
+
+    position = nx.spring_layout(G)
+    nx.draw_networkx_edges(G, position)
+    nx.draw_networkx_nodes(G, position, node_size=3000)
+
+    labels = {}
+    for node in G.nodes:
+        wrapped_text = "\n".join(textwrap.wrap(node, width=20))
+        labels[node] = wrapped_text
+    nx.draw_networkx_labels(
+        G, position, labels, font_size=6, font_color="white", verticalalignment="center"
+    )
+
+    plt.title(f"Star Graph for DrugBank ID: {drug_id}")
+    plt.show()
