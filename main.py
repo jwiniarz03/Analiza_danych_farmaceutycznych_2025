7a2f9f9136c20aa347b416fb5baac99d4e16c4f8
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-14 19:24:05 +0100

    added loading and parsing pathways data and dataframe of those data

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ from data_processing.data_loader import (
     load_basic_drug_data,
     load_drug_synonyms_data,
     load_products_data,
+    load_pathways_data,
 )
 from data_processing.data_frames import (
     create_data_frame_basic_info,
     create_data_frame_synonyms,
     create_data_frame_products,
+    create_data_frame_pathways,
 )
 from visualisations.graphs import generate_synonyms_graph, plot_synonyms_graph
 
@@ -30,3 +32,9 @@ if __name__ == "__main__":
     products_data = load_products_data(file_path)
 
     products_df = create_data_frame_products(products_data)
+
+    pathways_data = load_pathways_data(file_path)
+
+    pathways_df = create_data_frame_pathways(pathways_data)
+
+    print(pathways_df)
