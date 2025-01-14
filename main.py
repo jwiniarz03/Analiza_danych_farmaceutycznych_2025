7fe5b52036f53f17a0eaec161ef308ae056cde72
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-14 15:35:44 +0100

    added generating graph and plot of synonyms

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ from data_processing.data_frames import (
     create_data_frame_basic_info,
     create_data_frame_synonyms,
 )
+from visualisations.graphs import generate_synonyms_graph, plot_synonyms_graph
 
 if __name__ == "__main__":
 
     file_path = "drugbank_partial.xml"
+    drug_id = "DB00006"
 
-    # Load and parse data
     drugs_data = load_basic_drug_data(file_path)
 
     drug_df = create_data_frame_basic_info(drugs_data)
@@ -16,3 +17,7 @@ if __name__ == "__main__":
     synonyms_data = load_drug_synonyms_data(file_path)
 
     synonyms_df = create_data_frame_synonyms(synonyms_data)
+
+    G = generate_synonyms_graph(drug_id, synonyms_df)
+
+    plot_synonyms_graph(G, drug_id)
