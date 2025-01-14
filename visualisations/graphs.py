03f1b880c61be5d6972035715c14cac539f12dd4
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-14 10:18:24 +0100

    added function generating synonyms graph

--- visualisations/graphs.py
+++ visualisations/graphs.py
@@ -13,8 +13,24 @@ def generate_synonyms_graph(drug_id: str, synonyms_df: pd.DataFrame) -> nx.Graph
 
     Returns:
         nx.Graph: The generated star graph of synonyms.
+
+    Raises:
+        ValueError: If no synonyms are found for the given DrugBank ID.
     """
+
+    row = synonyms_df[synonyms_df["drug_id"] == drug_id]
+
+    if row.empty:
+        raise ValueError(f"No synonyms found for DrugBank ID {drug_id}")
+
+    synonyms = row.iloc[0]["synonyms"].split("\n")
 
     G = nx.Graph()
 
-    synonyms = []
+    G.add_node(drug_id)
+
+    for synonym in synonyms:
+        G.add_node(synonym)
+        G.add_edge(drug_id, synonym)
+
+    return G
