64603071bbbc1f08a9b3d3b9fcb92ab10e427bdb
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-19 23:09:47 +0100

    added class drug

---
+++ src/drugs.py
@@ -0,0 +1,33 @@
+class Drug:
+
+    def __init__(
+        self,
+        name: str,
+        drug_id: str,
+        drug_type: str,
+        description: str,
+        state: str,
+        indication: str,
+        mechanism_of_action: str,
+        food_interactions: str,
+    ):
+        self.name = name
+        self.drug_id = drug_id
+        self.drug_type = drug_type
+        self.description = description
+        self.state = state
+        self.indication = indication
+        self.mechanism_of_action = mechanism_of_action
+        self.food_interactions = food_interactions
+
+    def to_dict(self) -> dict:
+        return {
+            "DrugBank ID": self.drug_id,
+            "Name": self.name,
+            "Type": self.drug_type,
+            "Description": self.description,
+            "Form": self.state,
+            "Indications": self.indication,
+            "Mechanism_of_action": self.mechanism_of_action,
+            "Food_interactions": self.food_interactions,
+        }
