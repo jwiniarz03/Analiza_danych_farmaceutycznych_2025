fc47d23692f2b700a1a349242c545ee0699f9073
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-19 23:11:32 +0100

    deleted charts and moved it to separated file

--- visualisations/graphs.py
+++ visualisations/graphs.py
@@ -124,38 +124,3 @@ class BipartiteGraph:
         print("Adjacency List:")
         for vertex, neighbors in self.adj_list.items():
             print(f"{vertex}: {neighbors}")
-
-
-def create_pie_plot_targets(df: pd.DataFrame) -> plt.pie:
-    agregated_data = (
-        df.groupby("Cellular location")
-        .agg(nr_of_targets=("DrugBank ID", "count"))
-        .reset_index()
-    )
-    total_targets = len(df.index)
-    agregated_data["Percentage"] = (
-        agregated_data["nr_of_targets"] / total_targets
-    ) * 100
-
-    data = agregated_data["Percentage"]
-    locations = agregated_data["Cellular location"]
-    filtered_data = agregated_data[agregated_data["Percentage"] > 3]
-    filtered_labels = filtered_data["Cellular location"]
-
-    plt.figure(figsize=(10, 8))
-    plt.pie(
-        data,
-        labels=[
-            label if label in filtered_labels.values else "" for label in locations
-        ],
-        autopct=lambda p: f"{p:.1f}%" if p > 3 else "",
-        startangle=90,
-    )
-    plt.title("Distribution of Cellular Locations")
-    plt.legend(
-        locations,
-        title="Cellular Locations",
-        loc="upper left",
-        fontsize="small",
-    )
-    plt.show()
