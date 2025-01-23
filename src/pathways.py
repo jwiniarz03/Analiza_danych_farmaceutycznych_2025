908ca55e95d400a3100603ac427d094feb29009a
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-23 15:58:36 +0100

    added enzymes and changed drugs to List[str] to have only drugs names instead of drug objects

--- src/pathways.py
+++ src/pathways.py
@@ -4,16 +4,20 @@ from src.drugs import Drug
 
 class Pathway:
 
-    def __init__(self, id: str, name: str, category: str, drugs: List[Drug]):
+    def __init__(
+        self, id: str, name: str, category: str, drugs: List[str], enzymes: List[str]
+    ):
         self.id = id
         self.name = name
         self.category = category
         self.drugs = drugs
+        self.enzymes = enzymes
 
     def to_dict(self) -> dict:
         return {
             "Path_ID": self.id,
             "Name": self.name,
             "Category": self.category,
-            "Drugs": ", ".join([drug.name for drug in self.drugs]),
+            "Drugs": ", ".join(self.drugs),
+            "Enzymes": ", ".join(self.enzymes),
         }
