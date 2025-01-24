9256033755a4a2b5ad4f7b9042d9720a06d6dca1
jwiniarz03 <winiarz.julia@icloud.com>
2025-01-24 20:10:07 +0100

    added function to plot pathways histogram

--- visualisations/charts.py
+++ visualisations/charts.py
@@ -1,4 +1,27 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_pathways_histogram(df: pd.DataFrame):
+    """
+    Creates a histogram for data from a given DataFrame.
+
+    Args:
+        df (pd.DataFrame): DataFrame with drug_id and its number of pathways.
+    """
+
+    if df.empty:
+        raise ValueError("Given DataFrame is empty. No data to plot.")
+
+    plt.figure(figsize=(18, 8))
+    plt.bar(df["Drug"], df["Nr_of_pathways"], color="green")
+    plt.xlabel("Drug", fontsize=12, fontweight="bold")
+    plt.ylabel("Number of Pathways", fontsize=12, fontweight="bold")
+    plt.title("Number of Pathways for each Drug", fontsize=14, fontweight="bold")
+    plt.xticks(rotation=90, fontsize=8, fontweight="bold", ha="center")
+    plt.xlim(-0.5, len(df["Drug"]) - 0.5)
+    plt.tight_layout()
+    plt.show()
 
 
 def create_pie_plot_targets(df: pd.DataFrame) -> plt.pie:
